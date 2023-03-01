8139c12f70192a69cb6e98500f8436d15ae35e2a
Ganeshghatti <107616019+Ganeshghatti@users.noreply.github.com>
2023-03-01 09:29:13 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
-document.getElementsByClassName("displayquantity")[0].innerHTML="0"
+a = 0
+document.getElementsByClassName("displayquantity")[0].innerHTML = "0"
+
+
+document.getElementsByClassName("add")[0].addEventListener("click", add);
+
+function add() {
+    document.getElementsByClassName("displayquantity")[0].innerHTML = ++a
+}
+
+document.getElementsByClassName("remove")[0].addEventListener("click", subtract);
+
+function subtract() {
+    if (a == 0) {
+        document.getElementsByClassName("displayquantity")[0].innerHTML = 0
+    }
+    else {
+        document.getElementsByClassName("displayquantity")[0].innerHTML = --a
+    }
+}
+
